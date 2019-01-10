54ff1dd1d497b74e080856b4432d3dad42778dbd
the-machine-learner <ritvikush@gmail.com>
2019-01-10 22:12:29 +0530

    correction is post/users

--- server/server.js
+++ server/server.js
@@ -25,6 +25,7 @@ app.post('/todos',authentication,(req,res)=>{
 	var todo = new Todo({
 		text: req.body.text,
 		_creator: req.user._id
+
 	});
 	todo.save().then((doc)=>{
 		res.send(doc);
@@ -104,11 +105,10 @@ app.patch('/todos/:id',(req,res)=>{
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
-
-  user.save().then(() => {
+    user.save().then(() => {
     return user.generateAuthToken();
   }).then((token) => {
-
+    res.header('x-auth', token).send(user);	
   }).catch((e) => {
     res.status(400).send(e);
   });
